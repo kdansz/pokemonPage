272e115993482d78a79fa3ae295aad38b109a50b
kdansz <danielcrocci@hotmail.com>
2024-01-27 23:17:11 -0300

    Pokemon Site

---
+++ script.js
@@ -0,0 +1,41 @@
+const abrirDados = document.querySelector(".button1")
+
+const form = document.querySelector(".form1")
+
+const mascara = document.querySelector(".mascara-form")
+
+function inserirDados(){
+
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    form.style.transition = "1.5s, easy-in-out"
+    mascara.style.visibility = "visible"
+    mascara.style.transition = "1.5s, easy-in-out"
+
+
+}
+
+function esconderMascara(){
+
+    mascara.style.visibility = "hidden"
+    form.style.left = "-369px"
+    form.style.transform = "translateX(0)"
+    form
+}
+
+
+abrirDados.addEventListener("click", inserirDados)
+
+mascara.addEventListener("click", esconderMascara)
+
+const enviarDados = document.querySelector(".button2")
+
+function Dados(){
+
+    console.log(form.innerHTML)
+
+
+}
+
+
+enviarDados.addEventListener("click", Dados)
